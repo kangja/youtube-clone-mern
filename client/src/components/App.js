dd057d6d056f17f6006be4951253053ec11f0692
hi <kangja1996@gmail.com>
2020-09-26 16:12:58 -0400

    in pgoress

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,7 +7,7 @@ import LoginPage from "./views/LoginPage/LoginPage.js";
 import RegisterPage from "./views/RegisterPage/RegisterPage.js";
 import NavBar from "./views/NavBar/NavBar";
 import Footer from "./views/Footer/Footer"
-
+import UploadVideoPage from "./views/UploadVideoPage/UploadVideoPage"
 //null   Anyone Can go inside
 //true   only logged in user can go inside
 //false  logged in user can't go inside
@@ -21,6 +21,10 @@ function App() {
           <Route exact path="/" component={Auth(LandingPage, null)} />
           <Route exact path="/login" component={Auth(LoginPage, false)} />
           <Route exact path="/register" component={Auth(RegisterPage, false)} />
+
+          {/* it means the route is localhost:3000/video/upload */}
+          <Route exact path="/video/upload" component={Auth(RegisterPage, false)} />
+
         </Switch>
       </div>
       <Footer />
